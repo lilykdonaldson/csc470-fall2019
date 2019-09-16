820c12135aae3b5b83ed6b380722a1e9b76d6b14
Lily Donaldson <lilykdonaldson@gmail.com>
2019-09-16 16:27:45 -0400

    past exe

---
+++ exercises/Eating/Assets/Scripts/RatScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RatScript : MonoBehaviour
+{
+    float speed = 2f;
+    int haveRotate = 0;
+
+    //Set these references in the Unity inspector
+    //NOTE: You need to import the Unity UI package in order to reference the UI objects
+    //        See above where we say "using Unity.UI;"
+    public GameObject cheeseObj;
+    public Text gameLabelText;
+
+    private void Start()
+    {
+        gameLabelText.enabled=false;
+    }
+    void Update()
+    {
+        if (Time.fixedTime < 3.67)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+        else if (Time.fixedTime > 3.67 && haveRotate == 0)
+        {
+            transform.Rotate(0, 90, 0);
+            haveRotate += 1;
+        }
+        else if (Time.fixedTime > 3.67 && 5.6 > Time.fixedTime && haveRotate == 1)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+        else if (5.6 < Time.fixedTime && haveRotate == 1)
+        {
+            transform.Rotate(0, 90, 0);
+            haveRotate += 1;
+        }
+        else if (5.6 < Time.fixedTime && Time.fixedTime < 7.5 && haveRotate == 2)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+        else if (Time.fixedTime > 7.5 && haveRotate == 2)
+        {
+            transform.Rotate(0, -90, 0);
+            haveRotate += 1;
+        }
+        else if (Time.fixedTime > 7.5 && 9.6 > Time.fixedTime && haveRotate == 3)
+        {
+            transform.position = transform.position + (transform.forward * speed * Time.deltaTime);
+        }
+        else if (Time.fixedTime > 9.6 && haveRotate == 3)
+        {
+            transform.Rotate(0, 90, 0);
+            haveRotate += 1;
+        }
+        else if (Time.fixedTime > 9.4 && 10.5 > Time.fixedTime && haveRotate == 4)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+        else if (Time.fixedTime >10.5 && haveRotate == 4)
+        {
+            transform.Rotate(0, 90, 0);
+            haveRotate += 1;
+        }
+        else if (Time.fixedTime > 10.5 && 12.5 > Time.fixedTime && haveRotate == 5)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+        else if (Time.fixedTime > 12.5 && haveRotate == 5)
+        {
+            transform.Rotate(0, -90, 0);
+            haveRotate += 1;
+        }
+        else if (Time.fixedTime > 12.5 && 13.4 > Time.fixedTime && haveRotate == 6)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+        else if (13.4 < Time.fixedTime && haveRotate == 6)
+        {
+            transform.Rotate(0, -90, 0);
+            haveRotate += 1;
+        }
+        else if (Time.fixedTime > 13.4 && 15.2 > Time.fixedTime && haveRotate == 7)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+        else if (15.2 < Time.fixedTime && haveRotate == 7)
+        {
+            transform.Rotate(0, -90, 0);
+            haveRotate += 1;
+        }
+        else if (Time.fixedTime > 15.2 && 15.8 > Time.fixedTime && haveRotate == 8)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+        else if (15.8 < Time.fixedTime && haveRotate == 8)
+        {
+            transform.Rotate(0, 90, 0);
+            haveRotate += 1;
+        }
+        else if (15.8 < Time.fixedTime && haveRotate == 9)
+        {
+            Destroy(cheeseObj);
+            gameLabelText.enabled = true;
+        }
+
+    }
+}
---
+++ exercises/Roll a Ball/Assets/Scripts/LaunchScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LaunchScript : MonoBehaviour
+{
+    //This needs to be assigned a value in the Unity inspector. The "Rigidbody" component
+    //tells Unity to simulate the GameObject that is attached to be use a simulated behavior
+    public Rigidbody rigidbody;
+
+    //This (also assigned via the Unity inpector interface) is the UI component that displays
+    //the amount that we have charged. (in an unaesthetically pleasing way...)
+    public Text chargeEnergyText;
+
+    //This will ultimately scale the force from which the thing being launched is launched
+   // float chargeEnergy = 0;
+
+    //This controls the rate that chargeEnergy is accumulated
+    float chargeRate = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialize the UI component that displays the chargeEnergy
+        //chargeEnergyText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //When space is held, accumulate energy
+            //chargeEnergy += chargeRate * Time.deltaTime;
+
+            //Update the UI element that displays the chargeEnergy
+            //chargeEnergyText.text = chargeEnergy.ToString();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //Modify the rigidbody of the gameObject to use gravity
+            //(NOTE: This requires that you assigned the rigidbody variable in the Unity inspector)
+            rigidbody.useGravity = true;
+
+            //Apply the launch force!
+            rigidbody.AddForce(0, 300, 0);
+
+            //If this game supported it, this would make it so the next launch would 
+            //not carry with it the force of the previous charge.
+            //chargeEnergy = 0;
+        }
+    }
+}
