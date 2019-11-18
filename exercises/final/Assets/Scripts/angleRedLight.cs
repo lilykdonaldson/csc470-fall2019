920d1b143bb59988d1a6217fbe0b77383cd9b2dd
Lily Donaldson <lilykdonaldson@gmail.com>
2019-11-18 17:20:30 -0500

    swatchbook

---
+++ exercises/final/Assets/Scripts/angleRedLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class angleRedLight : MonoBehaviour
+{
+    public Slider angleSlider;
+    public Slider colorSlider;
+    public Light lt;
+
+    //Invoked when a submit button is clicked.
+   
+    // Use this for initialization
+    void Start()
+    {
+        //colorSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lt.spotAngle = angleSlider.value;
+        Color x = Color.HSVToRGB(colorSlider.value, 1, 1);
+        lt.color = x;
+    }
+
+}
+
